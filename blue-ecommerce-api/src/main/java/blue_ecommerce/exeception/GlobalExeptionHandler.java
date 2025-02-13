96623bf3c1b7340d930cb1afdd4fd76df585fff4
Walisson oliveira <walissonoliveira1@outlook.com>
2025-02-13 18:26:28 -0300

    Tratamento de erros

--- blue-ecommerce-api/src/main/java/blue_ecommerce/exeception/GlobalExeptionHandler.java
+++ blue-ecommerce-api/src/main/java/blue_ecommerce/exeception/GlobalExeptionHandler.java
@@ -3,8 +3,10 @@ package blue_ecommerce.exeception;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -19,5 +21,30 @@ public class GlobalExeptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->{
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        errors.put("type", "FORM_VALIDATION_ERROR");
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);    }
+
+@ExceptionHandler(DataIntegrityViolationException.class)
+public ResponseEntity<Map<String, String>> handleDataIntegrityViolation(DataIntegrityViolationException ex){
+    Map<String, String> errorResponse = new HashMap<>();
+    errorResponse.put("message", "Ocorreu um erro interno no servidor");
+    errorResponse.put("type", "INTERNAL_ERROR");
+    return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+}
+
+@ExceptionHandler(Exception.class)
+public ResponseEntity<Map<String, String>> handleGeneralExceptions(Exception ex){
+    Map<String, String> errorResponse = new HashMap<>();
+    errorResponse.put("message","Ocorreu um erro interno no servidor");
+    errorResponse.put("type", "INTERNAL_ERROR");
+    return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+}
+    
 
 }
