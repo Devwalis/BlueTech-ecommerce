40a488dfaa65b6b17a404d2012da15d29d615942
Walisson oliveira <walissonoliveira1@outlook.com>
2025-02-13 16:35:52 -0300

    ativação do cors para conexão de front e back-end

--- blue-ecommerce-api/src/main/java/blue_ecommerce/config/WebConfig.java
+++ blue-ecommerce-api/src/main/java/blue_ecommerce/config/WebConfig.java
@@ -1,7 +1,17 @@
 package blue_ecommerce.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-public class WebConfig {
+@Configuration
+public class WebConfig  implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:8080")
+            .allowedMethods("*")
+            .allowedHeaders("*");
+    }
     
 }
