bd21db921a824be7a6b73090fcd9bbab66f6701b
Walisson oliveira <walissonoliveira1@outlook.com>
2025-04-25 16:07:55 -0300

    Entidades produto e categoria

---
+++ produtos-api/produtos-api/src/main/java/com/ecommerce/produtos_api/model/Produto.java
@@ -0,0 +1,40 @@
+package com.ecommerce.produtos_api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+
+@Entity(name = "tb_produtos")
+public class Produto {
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String codigo;
+
+
+    @Column(nullable = false)
+    private String nome;
+
+
+    @Column(nullable = false)
+    private String descrição;
+    
+
+    @Column(nullable = false)
+    private Double precoDouble;
+
+    @ManyToOne
+    @JoinColumn Categoria categoria;
+
+
+
+}
