6715312abd47b467b13e789b929ed23ae7d07068
Walisson oliveira <walissonoliveira1@outlook.com>
2025-04-30 03:06:24 -0300

    Correção de código

--- produtos-api/produtos-api/src/main/java/com/ecommerce/produtos_api/dto/ProdutoDTO.java
+++ produtos-api/produtos-api/src/main/java/com/ecommerce/produtos_api/dto/ProdutoDTO.java
@@ -1,10 +1,7 @@
 package com.ecommerce.produtos_api.dto;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,33 +9,30 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(name = "tb_produtos")
+
 public class ProdutoDTO {
 
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Long id;
     
-    @Column(nullable = false, unique = true)
     private String codigo;
 
-    @Column(nullable = false, unique = true)
+   
     private String nome;
     
-    @Column(nullable = false, columnDefinition = "TEXT")
+   
     private String descricao;
 
-    @Column(nullable = false)
     private Double preco;
 
-    @Column(nullable = false)
+    
     private String Categoria;
 
-    @Column(nullable = false)
+
     private String Caracteristicas;
 
-    @Column(nullable = false)
+   
     private String avaliacoes;
 
     
