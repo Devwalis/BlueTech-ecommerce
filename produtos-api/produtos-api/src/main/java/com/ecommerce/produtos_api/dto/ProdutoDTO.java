76a458d5e3ffd39b14231a353e5b5845e1a64907
Walisson oliveira <walissonoliveira1@outlook.com>
2025-04-29 23:13:52 -0300

    Anotações para ORM

--- produtos-api/produtos-api/src/main/java/com/ecommerce/produtos_api/dto/ProdutoDTO.java
+++ produtos-api/produtos-api/src/main/java/com/ecommerce/produtos_api/dto/ProdutoDTO.java
@@ -1,5 +1,10 @@
 package com.ecommerce.produtos_api.dto;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,15 +12,33 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "tb_produtos")
 public class ProdutoDTO {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(nullable = false, unique = true)
     private String codigo;
+
+    @Column(nullable = false, unique = true)
     private String nome;
+    
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String descricao;
+
+    @Column(nullable = false)
     private Double preco;
+
+    @Column(nullable = false)
     private String Categoria;
+
+    @Column(nullable = false)
     private String Caracteristicas;
+
+    @Column(nullable = false)
     private String avaliacoes;
 
     
